a7bbfea231e546da87ad441147e272f8d2fd7c45
neethu23 <neethusnair23@gmail.com>
2016-09-29 16:54:08 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,16 @@
 console.log('Loaded!');
 
+//change text
 var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
+//move img
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginleft = marginleft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft = '100px';
+   var interval = setInterval(moveRight, 50);
 };
